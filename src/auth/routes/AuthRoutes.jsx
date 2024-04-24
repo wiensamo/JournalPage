45b5cf28f8b8dc384ccc33ced3c022022dcb2316
Wilson Sanchez <wiensamo@hotmail.com>
2024-04-23 20:48:18 -0500

    Add styles with materia en config al envoiriment for used

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -6,7 +6,7 @@ export const AuthRoutes = () => {
     <Routes>
         <Route path="login" element={ <LoginPage/> } />
         <Route path="register" element={ <RegisterPage/> } />
-        <Route path="/" element={ <Navigate  to="/auth/login"/>} />
+        <Route path='/*' element={ <Navigate  to="/auth/login"/>} />
     </Routes>
   )
 }
--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,10 +1,10 @@
-import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-import { pupleTheme } from './pupleTheme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { pupleTheme } from './';
 
 export const AppTheme = ({ children }) => {
   return (
-        <ThemeProvider theme={pupleTheme}>
+        <ThemeProvider theme={ pupleTheme }>
              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
             { children }
